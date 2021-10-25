e279b52b360031e03c6b81f9c61e1f1876234e5e
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-25 17:07:28 -0300

    compotamento de classe e Atributos Privados

--- Projetos/CaixaEletronico/index.js
+++ Projetos/CaixaEletronico/index.js
@@ -1,27 +1,45 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente{
+    agencia;
+    _saldo = 0;
 
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
+    }
+}
+const cliente1 = new Cliente();
 cliente1.nome = "jefferson";
 cliente1.cpf = 12345678999;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 
+const cliente2 = new Cliente();
 cliente2.nome = "jasmine";
 cliente2.cpf = 12345678988;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 12345678977;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
