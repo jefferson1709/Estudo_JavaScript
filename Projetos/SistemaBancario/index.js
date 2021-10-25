866b567c8f46ce682de756358e3a9f588463d515
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-25 17:51:44 -0300

    Metodos Abstratos

--- Projetos/SistemaBancario/index.js
+++ Projetos/SistemaBancario/index.js
@@ -1,18 +1,16 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 
 const cliente1 = new Cliente("jefferson", 12345678999);
 //const cliente2 = new Cliente("jasmine", 12345678988);
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
-
+const contaCorrente = new ContaCorrente( cliente1, 1001);
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
 
-
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo)
+console.log(contaSalario);
