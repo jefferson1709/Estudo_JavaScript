2d57488a4ed260e279656c042599c69855d0126c
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-25 17:45:17 -0300

    Herança

--- Projetos/SistemaBancario/index.js
+++ Projetos/SistemaBancario/index.js
@@ -1,16 +1,18 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import {Conta} from "./Conta.js";
+
 
 const cliente1 = new Cliente("jefferson", 12345678999);
 //const cliente2 = new Cliente("jasmine", 12345678988);
 
-const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
+
 
 console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
+console.log(contaCorrenteRicardo)
