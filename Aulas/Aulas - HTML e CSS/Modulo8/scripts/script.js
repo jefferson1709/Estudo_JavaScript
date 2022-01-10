ccaddfce63478ffb7c846e8b04d063fc3afb3896
Jefferson dos santos <Jefferson1709@hotmail.com>
2022-01-10 12:23:15 -0300

    aulas de HMTL intemediario: canva animação

--- Aulas/Aulas - HTML e CSS/Modulo8/scripts/script.js
+++ Aulas/Aulas - HTML e CSS/Modulo8/scripts/script.js
@@ -55,3 +55,38 @@ ctx.arc(x, y, raio, inicio, fim);
 
 ctx.fill()
 ctx.stroke();
+
+
+
+let circle = {
+    x: 250, 
+    y: 250,
+    raio: 100,
+    inicio: 0,
+    fim: 0,
+    antiHor: true,
+}
+
+function drawCircle(c){
+
+    ctx.beginPath();
+    ctx.rect(0,0,500,500)
+    ctx.fillStyle= "beige";
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle= "red";
+    ctx.fillStyle= "blue";
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim, c.antiHor);
+    ctx.fill();
+    ctx.stroke();
+}
+
+setInterval(function(){
+    if(circle.fim < 2 * Math.PI){
+        circle.fim += 0.3;
+        circle.x += 3;
+    }
+    drawCircle(circle);
+},250)
