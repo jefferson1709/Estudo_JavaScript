84a64cf166869d8d3f6fe975e886bfd6e559ebe4
jefferson1709 <jefferson1709@hotmail.com>
2022-01-15 16:33:31 -0300

    projeto jogo da memoria aula05

--- Projetos/Jogo da Memoria/scripts/script.js
+++ Projetos/Jogo da Memoria/scripts/script.js
@@ -10,7 +10,7 @@ function startGame(){
 
 function initializeCards(cards){
     let gameBoard = document.getElementById("gameBoard");
-
+    gameBoard.innerHTML='';
     game.cards.forEach(card => {
         let cardElement = document.createElement('div');
         cardElement.id = card.id;
@@ -43,10 +43,32 @@ function createCardFace(face, card, element){
     element.appendChild(cardElementFace);
 }
 
-
-
-
 function flipCard(){
-    this.classList.add("flip");
+    if(game.setCard(this.id)){
+        this.classList.add("flip");
+        if (game.secondCard){
+            if(game.checkMatch()){
+                game.clearCards();
+                if(game.checkGameOver()){
+                    let gamerOverLayer = document.getElementById("gameOver");
+                    gamerOverLayer.style.display = 'flex'
+                }
+            }else{
+                setTimeout(()=>{
+                let firstCardView = document.getElementById(game.firstCard.id);
+                let secondCardView = document.getElementById(game.secondCard.id);
+                firstCardView.classList.remove("flip");
+                secondCardView.classList.remove("flip");
+                game.unflippedCards();
+                },1000)
+            }
+        }
+    }
+}
 
+function restart(){
+    game.clearCards();
+    startGame();
+    let gamerOverLayer = document.getElementById("gameOver");
+    gamerOverLayer.style.display = 'none';
 }
