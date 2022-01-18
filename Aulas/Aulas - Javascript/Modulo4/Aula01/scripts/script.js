06b69267afe76097cf7c36a9a813061da257fc69
jefferson1709 <jefferson1709@hotmail.com>
2022-01-18 20:15:40 -0300

    Jquery Aula 01

---
+++ Aulas/Aulas - Javascript/Modulo4/Aula01/scripts/script.js
@@ -0,0 +1,44 @@
+
+$(document).mouseleave(() =>{
+    $("#sair").show();
+})
+
+/*
+console.log($("h1"))
+
+$(".titulo").hide();
+
+setTimeout(()=>{$(".titulo").show();},2000)
+
+//$.noConflict();
+//jQuery(".titulo").hide();
+
+//let jq = $.noConflict();
+//jq(".titulo").hide();
+
+//JavaScript vanilla
+//document.querySelectorAll(".titulo").forEach(elemento=>{
+//	elemento.style.display = "none";
+//})
+
+
+$(".titulo").click(() =>{
+    console.log("click")
+})
+
+$(".titulo").mouseenter(() =>{
+    console.log("entrou com o mouse")
+})
+$(".titulo").mouseleave(() =>{
+    console.log("saiu com o mouse")
+})
+$(".titulo").dblclick(() =>{
+    console.log("clickou 2x")
+})
+
+//informações do evento
+$(".titulo").click((e) =>{
+    console.log(e)
+})
+
+*/
