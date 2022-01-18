d714d04e39c9177053a09114b06f00e59882a637
jefferson1709 <jefferson1709@hotmail.com>
2022-01-18 20:16:49 -0300

    Jquery Aula 03

---
+++ Aulas/Aulas - Javascript/Modulo4/Aula03/scripts/script.js
@@ -0,0 +1,93 @@
+
+
+
+/*
+$("#lista1 > .item2").hide();
+
+let lista1=$("#lista1");
+lista1.find(".item2").hide();
+lista1.children(".item2").hide();
+
+
+console.log(body.children("#lista1"));
+console.log(body.find("#lista1"));
+
+console.log(body.children(".item2"));
+console.log(body.find(".item2"));
+
+let body=$("body");
+console.log(body.find("#lista1"));
+
+let jq = $("#lista1");
+console.log(jq);
+
+let js = document.getElementById("lista1")
+console.log(js);
+
+let jq = $("ul");
+console.log($());
+console.log(jq);
+
+let jq = $("#lista1").slideUp(2000);
+jq.slideDown(2000)
+console.log(jq);
+
+let jq = $("#lista1").slideUp(2000).slideDown(2000).fadeOut(1000);
+console.log(jq);
+
+let objeto ={
+    print10: function(){
+        console.log(10);
+        return this;
+    },
+    print20: function(){
+        console.log(20);
+        return this;
+    },
+}
+
+let eleMesmo = objeto.print10();
+eleMesmo.print20();
+
+objeto.print10().print10().print10().print20();
+
+console.log($("h1").text());
+console.log($("#lista1").text());
+console.log($("#lista1").html());
+
+console.log($("#novo").attr("name"));
+
+console.log($("h1").text("Lista de Compras"));
+
+console.log($("#lista1").find(".item2").text("novo item"));
+
+$("li").attr("name",function(i, oValue){
+    return oValue + i;
+})
+
+console.log($("[Key = 2]").remove())
+
+$("#lista1").find("[Key = 2]").remove()
+
+$("li").remove(".item2, .item4");
+$("li").empty(".item2, .item4");
+
+let lista1 = $("#lista1")
+let items = lista1.html()
+lista1.html(items + "<li>TESTE</li>");
+
+let lista1 = $("#lista1")
+lista1.append("<li>TESTE</li>");
+lista1.prepend("<li>TESTE</li>");
+
+lista1.after("<li>TESTE</li>");
+lista1.before("<li>TESTE</li>");
+
+let rect = $(".rect")
+rect.append("Novo teste append");
+rect.prepend("Novo teste prepend");
+rect.after("Novo teste after");
+rect.before("Novo teste before");
+
+*/
+
