637c5f1036316646669f9090e02816058fd8694b
jefferson.fesa <jefferson.santos@fesagroup.com>
2022-11-01 23:08:13 -0300

    Revisao_javascript_02.2

---
+++ Aulas/Aulas - Javascript/Revisao 2022/02/script.js
@@ -0,0 +1,17 @@
+ //while = enquato
+ /*
+ var x = 5;
+ while(x< 10){
+  document.write("<br> O valor do X é: " + x);
+
+  //Aumentando o valor do x
+  x++;
+ }
+*/
+
+// for = para
+var valor = 15;
+for (a = 0; a< valor; a++){
+  document.write("<br> O valor do A é: " + x);
+  console.log(a+10)
+}
