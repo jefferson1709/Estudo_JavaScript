159ddc33519e0b6ebcaf578802084c21c5852aa7
jefferson.fesa <jefferson.santos@fesagroup.com>
2022-11-02 23:20:38 -0300

    Revisao_javascript_02.3

--- Aulas/Aulas - Javascript/Revisao 2022/02/script.js
+++ Aulas/Aulas - Javascript/Revisao 2022/02/script.js
@@ -10,8 +10,33 @@
 */
 
 // for = para
+ /*
 var valor = 15;
 for (a = 0; a< valor; a++){
   document.write("<br> O valor do A é: " + x);
   console.log(a+10)
+}
+*/
+
+// switch = comutador
+function pedir(){
+  var valor = prompt("Digite um valor de 1 a 4");
+
+  switch(Number(valor)){
+    case 1: 
+      alert("Voce escolheu 1 = Suco");
+      break;
+    case 2: 
+      alert("Voce escolheu 2 = Agua gelada");
+      break;
+    case 3: 
+      alert("Voce escolheu 3 = Sorvete");
+      break;
+    case 4: 
+      alert("Voce chamou o garcom");
+      break;
+    default: 
+      alert("escolha uma opcao entre 1 a 4");
+      break;
+  }
 }
